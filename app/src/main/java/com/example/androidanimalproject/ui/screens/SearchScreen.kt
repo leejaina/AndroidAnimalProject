10e2bd28e13d824fa817bafafe88e565e68bc7bb
leejaina <leejaina@konkuk.ac.kr>
2025-04-06 22:38:38 +0900

    feat: SearchScreen 의 AddButton Composable 만듬

--- app/src/main/java/com/example/androidanimalproject/ui/screens/SearchScreen.kt
+++ app/src/main/java/com/example/androidanimalproject/ui/screens/SearchScreen.kt
@@ -1,2 +1,37 @@
 package com.example.androidanimalproject.ui.screens
 
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun AddButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    FloatingActionButton(
+        onClick = onClick,
+        containerColor = Color(0xFFFFA938),
+        modifier = modifier
+    ) {
+        Icon(
+            imageVector = Icons.Default.Add,
+            contentDescription = "추가",
+            tint = Color.White,
+            modifier = Modifier.size(48.dp)
+        )
+    }
+}
+
+@Preview
+@Composable
+fun AddButtonPreview() {
+    AddButton(onClick = {})
+}
